a29858035058dbccb28e6e8a769586f4ccb46478
otisbrother <huytoa3@gmail.com>
2024-08-01 09:13:21 +0700

    Initial commit

---
+++ event.js
@@ -0,0 +1,20 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let slides = document.getElementsByClassName("slide");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active");
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].classList.add("active");
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
+
+function changeSlide(n) {
+  slideIndex += n - 1;
+  showSlides();
+}
